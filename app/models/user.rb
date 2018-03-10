691f6b9b35f0b95f8b5e4126d3c5ecfdd5f63c4d
An Nuyen <annuyen@gmail.com>
2018-03-10 19:20:46 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
   has_many :posts, through: :comments
 
   def users_attributes=(user_attributes)
-    user_attributes.values.each do |user_attribute| 
-      user = User.find_or_create_by(user_attribute) 
-      self.users << user 
-    end 
-  end 
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.users << user
+    end
+  end
 end
