746e964cafedf23229ae7a084d9de7de53f34515
An Nuyen <annuyen@gmail.com>
2018-03-10 19:19:07 +0000

    automatically backed up by learn

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,9 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :post_categories
 
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute| 
+    category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
-      self.categories << category 
-    end 
-  end 
+      self.categories << category
+    end
+  end
 end
